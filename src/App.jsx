96405cf0392e0878a455a9acf05cdedc19370c75
Arhaan Siddiquee <siddiqueearhaan@gmail.com>
2025-03-06 07:32:33 +0530

    code editor added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Profile from './components/Education'
+import TerminalCodeEditor from './components/Terminal'
 
 const App = () => {
   return (
     <>
     
     <Profile/>
+    <TerminalCodeEditor/>
     </>
   )
 }
